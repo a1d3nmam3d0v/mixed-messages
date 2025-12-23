79f963b70e1890fa4c82563ff13026096d38ca47
a1d3nmam3d0v <89761421+a1d3nmam3d0v@users.noreply.github.com>
2025-12-23 08:43:43 -0600

    Refactor generateBritneyMoment function and update scenario generation logic

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-const looks = ["A pleated schoolgirl skirt and pink hair puffs", "A skin-tight red latex catsuit", "A live albino python draped over her shoulders", "A pink bob wig and big sunglasses", "A floor-length patchwork denim gown", "A sparkly ringmaster jacket and top hat", "A blue waitress uniform with a 'Britney' nametag", "A bridal veil and a 'Boy Toy' belt buckle"];
+const looks = ["A pleated schoolgirl skirt and pink hair puffs", "A skin-tight red latex catsuit", "A live albino python draped over her shoulders", "A pink bob wig and big sunglasses", "A floor-length patchwork denim gown", "A sparkly ringmaster jacket and top hat", "A grey hoodie, shorts, and a freshly-shaven bald head", "A bridal veil and a 'Boy Toy' belt buckle"];
 
-const locations = ["in a high school hallway", "on a desert-scape on Mars", "under the hot lights of the VMA stage", "at a gastation", "on the 2001 AMA red carpet", "in the center of a three-ring circus", "at a retro 1950s diner", "on top of a giant wedding cake"];
+const locations = ["in a high school hallway", "on a desert-scape on Mars", "under the hot lights of the VMA stage", "at a gastation", "on the 2001 AMA red carpet", "in the center of a three-ring circus", "at a gastation", "on top of a giant wedding cake"];
 
-const actions = ["hitting you baby one more time.", "dropping the Heart of the Ocean in the ocean.", "being a slave for you.", "making a 5AM Red Bull run.", "looking like a Y2K dream.", "cracking the whip like a master.", "making a scene in 'You Drive Me Crazy'.", "kissing Madonna."];
+const actions = ["hitting you baby one more time.", "playing with your heart.", "being a slave for you.", "making a 5AM Red Bull run.", "looking like a Y2K dream.", "cracking the whip like a master.", "brandishing an umbrella against paparazzi", "kissing Madonna."];
 
-let scenario = (arr) => {
+const getRandomElement = (arr) => {
+	const indx = Math.floor(Math.random() * arr.length);
+	return arr[indx];
+};
+
+const generateBritneyMoment = () => {
 	let scenario = [];
-	scenario.push(getRandomElement(arr));
+	scenario.push(getRandomElement(looks));
+	scenario.push(getRandomElement(locations));
+	scenario.push(getRandomElement(actions));
+	return scenario.join(" ");
 };
 
-const getRandomElement = (arr, arr2, arr3) => {
-	const look = Math.floor(Math.random() * arr.length);
-	const place = Math.floor(Math.random() * arr2.length);
-	const action = Math.floor(Math.random() * arr3.length);
-	return `${arr[look]} ${arr2[place]} ${arr3[action]}`;
-};
-
-getRandomElement(looks, locations, actions);
-
-console.log(getRandomElement(looks, locations, actions));
+console.log(generateBritneyMoment());
