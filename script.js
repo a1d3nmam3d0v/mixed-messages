82eb5672d0b4964de6798c5249b966ea5d3ade55
a1d3nmam3d0v <89761421+a1d3nmam3d0v@users.noreply.github.com>
2025-12-23 12:59:58 -0600

    Refactor randomizer and add win state logic

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-const looks = ["A pleated schoolgirl skirt and pink hair puffs", "A skin-tight red latex catsuit", "A live albino python draped over her shoulders", "A pink bob wig and big sunglasses", "A floor-length patchwork denim gown", "A sparkly ringmaster jacket and top hat", "A grey hoodie, shorts, and a freshly-shaven bald head", "A bridal veil and a 'Boy Toy' belt buckle"];
+const looks = ["A pleated schoolgirl skirt and pink hair puffs", "A skin-tight red latex catsuit", "A live albino python draped over her shoulders", "A pink bob wig and big sunglasses", "A floor-length patchwork denim gown", "A sparkly ringmaster jacket and top hat", "A grey hoodie and freshly-shaved bald head", "A bridal veil and a 'Boy Toy' belt buckle"];
 
-const locations = ["in a high school hallway", "on a desert-scape on Mars", "under the hot lights of the VMA stage", "at a gastation", "on the 2001 AMA red carpet", "in the center of a three-ring circus", "at a gastation", "on top of a giant wedding cake"];
+const locations = ["at a high school", "on a desert-scape on Mars", "under the hot lights of the VMA stage", "at a gas station", "on the 2001 AMA red carpet", "in the center of a three-ring circus", "at a gas station", "on top of a giant wedding cake"];
 
-const actions = ["hitting you baby one more time.", "playing with your heart.", "being a slave for you.", "making a 5AM Red Bull run.", "looking like a Y2K dream.", "cracking the whip like a master.", "brandishing an umbrella against paparazzi", "kissing Madonna."];
-
-const getRandomElement = (arr) => {
-	const indx = Math.floor(Math.random() * arr.length);
-	return arr[indx];
-};
+const actions = ["hitting you baby one more time.", "playing with your heart...oops!", "being a slave for you.", "making a 5AM Red Bull run.", "looking like a Y2K dream.", "cracking the whip like a master.", "brandishing an umbrella against paparazzi.", "kissing Madonna."];
 
 const generateBritneyMoment = () => {
-	let scenario = [];
-	scenario.push(getRandomElement(looks));
-	scenario.push(getRandomElement(locations));
-	scenario.push(getRandomElement(actions));
-	return scenario.join(" ");
+	const lookIndex = Math.floor(Math.random() * looks.length);
+	const locationIndex = Math.floor(Math.random() * locations.length);
+	const actionIndex = Math.floor(Math.random() * actions.length);
+
+	let message = `${looks[lookIndex]} ${locations[locationIndex]} ${actions[actionIndex]}`;
+
+	console.log(message);
+
+	if (lookIndex === locationIndex && locationIndex === actionIndex) {
+		console.log(`\n Work, bitch! That was such an iconic moment in pop history.`);
+	} else {
+		console.log(`\n Not quite a match but still iconic.`);
+	}
 };
 
-console.log(generateBritneyMoment());
+generateBritneyMoment();
